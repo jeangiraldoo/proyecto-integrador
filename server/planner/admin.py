5b8c5a64d0cbd563416ecc199007627f695bf548
Jean Giraldo <jeann.giraldo@gmail.com>
2026-02-20 23:43:55 -0500

    feat(server): add and migrate models

--- server/planner/admin.py
+++ server/planner/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    User,
+    Activity,
+    Subtask,
+    Progress,
+    Conflict,
+    ConflictResolution,
+)
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Activity)
+admin.site.register(Subtask)
+admin.site.register(Progress)
+admin.site.register(Conflict)
+admin.site.register(ConflictResolution)
